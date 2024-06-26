567af4567ccfa85a53043e65939b1ad1b4e016d4
SgLgE <156662720+SgLgE@users.noreply.github.com>
2024-06-26 23:42:26 +0200

    Create question.js

---
+++ DetectiveGame/question.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: "Quelle balise utilise-t-on pour créer un lien hypertexte ?",
+        answer: "<a>",
+        code: "<a href='http://example.com'>Exemple</a>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour une image ?",
+        answer: "<img>",
+        code: "<img src='image.jpg' alt='Description'>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour créer un paragraphe ?",
+        answer: "<p>",
+        code: "<p>Ceci est un paragraphe.</p>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour un titre de niveau 1 ?",
+        answer: "<h1>",
+        code: "<h1>Titre de niveau 1</h1>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour une liste non ordonnée ?",
+        answer: "<ul>",
+        code: "<ul>\n    <li>Élément 1</li>\n    <li>Élément 2</li>\n</ul>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour une liste ordonnée ?",
+        answer: "<ol>",
+        code: "<ol>\n    <li>Élément 1</li>\n    <li>Élément 2</li>\n</ol>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour un champ de saisie de texte ?",
+        answer: "<input>",
+        code: "<input type='text' placeholder='Votre texte ici'>"
+    },
+    {
+        question: "Quelle balise utilise-t-on pour insérer une ligne horizontale ?",
+        answer: "<hr>",
+        code: "<hr>"
+    }
+];
